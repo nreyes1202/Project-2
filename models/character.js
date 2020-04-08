f6406fc6a0f7fb5584bdf986445734efc2dca054
Nilsen <58581719+nreyes1202@users.noreply.github.com>
2020-04-08 15:26:21 -0600

    synced to sequelize

---
+++ models/character.js
@@ -0,0 +1,20 @@
+// Dependencies
+// =============================================================
+
+// Sequelize (capital) references the standard library
+var Sequelize = require("sequelize");
+// sequelize (lowercase) references our connection to the DB.
+var sequelize = require("../config/config.js");
+
+// Creates a "Character" model that matches up with DB
+var Character = sequelize.define("character", {
+  // the username (a string)
+  username: Sequelize.STRING,
+  highScore: Sequelize.STRING,
+});
+
+// Syncs with DB
+Character.sync();
+
+// Makes the Character Model available for other files (will also create a table)
+module.exports = Character;
