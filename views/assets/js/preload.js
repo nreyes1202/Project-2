773a2d2237047a672e0bfe6db4ca911bcdfeafb5
JeanPeel <Jeanine.Peel@gmail.com>
2020-04-09 19:35:17 -0600

    image tile blocks

---
+++ views/assets/js/preload.js
@@ -0,0 +1,10 @@
+var preloadFunction = {
+    function preload ()
+{
+    this.load.image('sky', 'assets/images/sky.png');
+    this.load.image('ground', 'assets/images/platform.png');
+    this.load.image('star', 'assets/images/star.png');
+    this.load.image('bomb', 'assets/images/bomb.png');
+    this.load.spritesheet('dude', 'assets/images/dude.png', { frameWidth: 32, frameHeight: 48 });
+}
+}
