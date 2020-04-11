31719cdfcbe333eaca797e8b107b5d9fdabc4995
Nilsen <58581719+nreyes1202@users.noreply.github.com>
2020-04-11 14:00:56 -0600

    updates api routes

--- models/character.js
+++ models/character.js
@@ -1,51 +1,27 @@
 // Dependencies
 // =============================================================
 
-// Sequelize (capital) references the standard library
-var Sequelize = require("sequelize");
-// sequelize (lowercase) references our connection to the DB.
-var sequelize = require("../config/config.js");
+module.exports = function(sequelize, Sequelize) {
 
-// Creates a "Character" model that matches up with DB
-// var Character = sequelize.define("character", {
-//   // the username (a string)
-//   id: {
-//     autoIncrement: true,
-//     primaryKey: true,
-//     type: Sequelize.INTEGER
-// },
-//   username: {type: Sequelize.STRING
-//   },
-//   highScore: {type: Sequelize.INTEGER
-//   }
-// });
+//Creates a "Character" model that matches up with DB
+var Character = sequelize.define("character", {
+  // the username (a string)
+  id: {
+    autoIncrement: true,
+    primaryKey: true,
+    type: Sequelize.INTEGER
+},
+  username: {type: Sequelize.STRING
+  },
+  highScore: {type: Sequelize.INTEGER
+  }
+});
 
-// Syncs with DB
-// Character.sync();
+//Syncs with DB
+Character.sync();
 
 // Makes the Character Model available for other files (will also create a table)
-// module.exports = Character;
+ return Character;
 
-module.exports = function(sequelize, DataTypes) {
-  var Character = sequelize.define("Post", {
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1]
-      }
-    },
-    body: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-      validate: {
-        len: [1]
-      }
-    },
-    category: {
-      type: DataTypes.STRING,
-      defaultValue: "Personal"
-    }
-  });
-  return Post;
-};
+}
+
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,57 +1,33 @@
 
 // Dependencies
 // Requiring our model
-// var db = require("../models");
+ var db = require("../models");
 
 // Routes
 module.exports = function(app) {
 
   // GET route for getting username
-  // app.get("/api/user/", function(req, res) {
-  //   db.username.findAll({})
-  //     .then(function(dbuserName) {
-  //       res.json(dbuserName);
-  //     });
-  //     console.log(dbuserName);
-  // });
+  app.get("/api/user/", function(req, res) {
+    db.Character.findAll({})
+      .then(function(dbCharacter) {
+        res.json(dbCharacter);
+      });
+  });
+
+//   // POST route for saving a new username
+  app.post("/api/posts", function(req, res) {
+    console.log(req.body);
+    db.Post.create({
+      title: req.body.title,
+      body: req.body.body,
+      category: req.body.category
+    })
+      .then(function(dbPost) {
+        res.json(dbPost);
+      });
+  });
 
 
-//   // Get route for retrieving a single post
-//   app.get("/api/posts/:id", function(req, res) {
-//     db.Post.findOne({
-//       where: {
-//         id: req.params.id
-//       }
-//     })
-//       .then(function(dbPost) {
-//         res.json(dbPost);
-//       });
-//   });
-
-//   // POST route for saving a new post
-//   app.post("/api/posts", function(req, res) {
-//     console.log(req.body);
-//     db.Post.create({
-//       title: req.body.title,
-//       body: req.body.body,
-//       category: req.body.category
-//     })
-//       .then(function(dbPost) {
-//         res.json(dbPost);
-//       });
-//   });
-
-//   // DELETE route for deleting posts
-//   app.delete("/api/posts/:id", function(req, res) {
-//     db.Post.destroy({
-//       where: {
-//         id: req.params.id
-//       }
-//     })
-//       .then(function(dbPost) {
-//         res.json(dbPost);
-//       });
-//   });
 
 //   // PUT route for updating posts
 //   app.put("/api/posts", function(req, res) {
